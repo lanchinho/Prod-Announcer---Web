e3f7028aafbc23ff73b90d16917989300cfa1a9a
Lanchinho <guilherme.albuquerque@uniriotec.br>
2014-01-08 00:16:49 -0200

    Consertos para fazer a página de cadastro de produto funcionar, consertos na mensagem de sucesso ao cadastrar um produto e pequena correção no menu =D

--- prod_announcer/produto/views.py
+++ prod_announcer/produto/views.py
@@ -5,17 +5,16 @@ from django.shortcuts import redirect, get_object_or_404
 from annoying.decorators import render_to
 
 from prod_announcer.produto.models import Produto
+
 from prod_announcer.produto.forms import ProdutoForm
 
 @render_to("cadastro_produto.html")
 def cadastrar_produto(request):
     
     if request.method == 'POST':
-    	form = ProdutoForm(request.Post)
-
+    	form = ProdutoForm(request.POST)
     	if form.is_valid():
     		form.save()
-
     		return redirect('sucesso')
 
     else:
