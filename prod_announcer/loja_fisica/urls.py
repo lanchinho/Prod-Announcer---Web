ac9513d04147dd6ef648a1698afa5d283d22137b
Lanchinho <guilherme.albuquerque@uniriotec.br>
2014-01-26 00:57:26 -0200

    Correções na pagina de sucesso de produtos e tentativas mau sucedidas de fazer mudança de senha

--- prod_announcer/loja_fisica/urls.py
+++ prod_announcer/loja_fisica/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.forms import AdminPasswordChangeForm
 from django.views.generic import TemplateView
 
 from prod_announcer.loja_fisica import views
@@ -8,4 +9,7 @@ urlpatterns = patterns('',
         url(r'^cadastro_loja/$', views.cadastrar_loja, name= 'cadastro_loja'),
 	    url(r'^redefinir_senha/$', TemplateView.as_view(template_name="redefinir_senha.html"), name='redefinir_senha'),
         url(r'^sucesso/$', TemplateView.as_view(template_name="sucesso.html"), name='sucesso'),
+        #url(r'^redefinir_senha/$', 'django.contrib.auth.views.password_change', name="redefinir_senha")
+        #url(r'^senha_alterada/$', 'django.contrib.auth.views.password_change_done', name="senha_alterada")
+    
 	)
